9b2143e04429e2348878a8e04a55195ff7b8b4b7
curious jeager <edam7635@gmail.com>
2019-05-10 01:42:39 +0900

    styled-component and router

---
+++ src/Components/GlobalStyles.js
@@ -0,0 +1,23 @@
+import {createGlobalStyle} from 'styled-components';
+import reset from 'styled-reset';
+
+const globalStyles = createGlobalStyle`
+    ${reset};
+    a{
+        text-decoration:none;
+        color:inherit;
+    }
+    *{
+        box-sizing:border-box;
+    }
+    body{
+        font-family:-apple-system,-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+        font-size:12px;
+        background-color:rgba(20, 20, 20, 1);
+        color:white;
+        padding-top:50px;
+    }
+
+`;
+
+export default globalStyles;
