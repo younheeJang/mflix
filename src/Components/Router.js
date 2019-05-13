d5e0f8102efb8b49039212b8360fc3fe1ceb0f34
curious jeager <edam7635@gmail.com>
2019-05-14 08:52:35 +0900

    tv home presenter

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -8,6 +8,7 @@ import {
 import Home from "Routes/Home/HomeContainer";
 import TV from "Routes/TV/TVContainer";
 import Search from "Routes/Search/SearchContainer";
+import Detail from "Routes/Detail/DetailContainer";
 import Header from 'Components/Header';
 
 export default () => (
@@ -18,6 +19,8 @@ export default () => (
       <Route path="/" exact component={Home} />
       <Route path="/tv" exact component={TV} />
       <Route path="/search" exact component={Search} />
+      <Route path="/movie/:id" exact component={Detail} />
+      <Route path="/tv/:id" exact component={Detail} />
       <Redirect from="*" to="/" />
     </Switch>
     </>
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,1 +1,16 @@
-export default () => 'Search Search';
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const SearchPresenter = ({movieResults, tvResults, loading, error, searchTerm, handleSubmit}) => null;
+
+SearchPresenter.PropTypes = {
+        movieResults:PropTypes.array,
+        tvResults:PropTypes.array,
+        loading:PropTypes.bool.isRequired,
+        error:PropTypes.string,
+        searchTerm:PropTypes.string,
+        handleSubmit:PropTypes.func.isRequired
+}
+
+export default SearchPresenter;
